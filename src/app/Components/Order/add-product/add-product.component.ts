5db6be3e26713d73d020fca9bd2c2748bcf61b0c
Yahia <mohamedyahia207545@gmail.com>
2023-06-12 01:29:46 +0300

    template driven form

--- src/app/Components/Order/add-product/add-product.component.ts
+++ src/app/Components/Order/add-product/add-product.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { IProduct } from 'src/app/Models/iproduct';
 import { ProductsService } from 'src/app/Services/products.service';
+import { ICategory } from '../../../Models/icategory';
+import { IProduct } from '../../../Models/iproduct';
 
 @Component({
   selector: 'app-add-product',
@@ -9,17 +10,25 @@ import { ProductsService } from 'src/app/Services/products.service';
   styleUrls: ['./add-product.component.scss'],
 })
 export class AddProductComponent {
-  constructor(private prodService: ProductsService, private router: Router) {}
+  catList: ICategory[];
+  newProd: IProduct = {} as IProduct;
+  constructor(private prodService: ProductsService, private router: Router) {
+    this.catList = [
+      { id: 1, name: 'Laptops' },
+      { id: 2, name: 'Tablets' },
+      { id: 3, name: 'Mobiles' },
+    ];
+  }
 
   addProduct() {
-    const product: IProduct = {
-      id: 100,
-      name: 'New Phone',
-      price: 500,
-      quantity: 500,
-      imgURL: '',
-      categoryID: 3,
-    };
+    // const product: IProduct = {
+    //   id: 100,
+    //   name: 'New Phone',
+    //   price: 500,
+    //   quantity: 500,
+    //   imgURL: '',
+    //   categoryID: 3,
+    // };
 
     // this.prodService.addProduct(product).subscribe((product) => {
     //   alert('Add product successfully'); // Not recommended
@@ -37,6 +46,6 @@ export class AddProductComponent {
         alert(err.message);
       },
     };
-    this.prodService.addProduct(product).subscribe(observer);
+    this.prodService.addProduct(this.newProd).subscribe(observer);
   }
 }
